a2e0e3004f42f9f2a91c2ae9c2587c3d65730b96
dllo <1586676476@qq.com>
2017-03-13 15:17:18 +0800

    no message

--- XMLY/app/src/main/java/com/jieleo/xmly_plus/adapter/discover_adapter/DisCoverAdapter.java
+++ XMLY/app/src/main/java/com/jieleo/xmly_plus/adapter/discover_adapter/DisCoverAdapter.java
@@ -19,7 +19,7 @@ import com.jieleo.xmly_plus.tools.BaseViewHolder;
  * Created by dllo on 17/3/10.
  */
 
-public class DisCoverAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class DisCoverAdapter extends RecyclerView.Adapter<BaseViewHolder> {
     private static final String TAG = "DisCoverAdapter";
     private DisCoverBean disCoverBean;
     private Context context;
@@ -39,97 +39,141 @@ public class DisCoverAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         this.context = context;
     }
 
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = null;
-        RecyclerView.ViewHolder holder = null;
-        switch (viewType) {
-            case ONE:
-                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
-                holder = new OneHolder(view);
-               return holder;
-            case TWO:
-                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
-                holder = new OneHolder(view);
-                return holder;
-            case THREE:
-                view = LayoutInflater.from(context).inflate(R.layout.discover_recycleview_item_two, parent, false);
-                holder = new TwoHolder(view);
-                return holder;
-            case FOUR:
-                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
-                holder = new OneHolder(view);
-                return holder;
-            case FIVE:
-                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item_three, parent, false);
-                holder = new ThreeHolder(view);
-                return holder;
-        }
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-        int type = getItemViewType(position);
-
-        switch (type) {
-//            case 0:
-//                OneHolder oneHolder = (OneHolder) holder;
-//                oneHolder.left.setText(disCoverBean.getList().get(0).getList().get(position).getTitle());
-//                oneHolder.right.setText(disCoverBean.getList().get(0).getList().get(position).getSubtitle());
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position).getCoverPath()).into(oneHolder.leftImgae);
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position).getSubCoverPath()).into(oneHolder.rightImage);
-//                Log.e(TAG, "onBindViewHolder: "+oneHolder.left);
-//                break;
-//            case 1:
-//                OneHolder oneHoldera = (OneHolder) holder;
-//                oneHoldera.left.setText(disCoverBean.getList().get(0).getList().get(position-1).getTitle());
-//                oneHoldera.right.setText(disCoverBean.getList().get(0).getList().get(position-1).getSubtitle());
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-1).getCoverPath()).into(oneHoldera.leftImgae);
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-1).getSubCoverPath()).into(oneHoldera.rightImage);
-//                Log.e(TAG, "onBindViewHolder: "+oneHoldera.leftImgae);
-//                break;
-//            case 2:
-//                break;
-//            case 3:
-//                OneHolder oneHolderc = (OneHolder) holder;
-//                oneHolderc.left.setText(disCoverBean.getList().get(0).getList().get(position-3).getTitle());
-//                oneHolderc.right.setText(disCoverBean.getList().get(0).getList().get(position-3).getSubtitle());
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-3).getCoverPath()).into(oneHolderc.leftImgae);
-//                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-3).getSubCoverPath()).into(oneHolderc.rightImage);
-//                break;
-//            case 4:
-//                break;
-
-        }
-    }
-
-
-
     @Override
     public int getItemViewType(int position) {
-        if (position == 0) {
-            return ONE;
-        } else if (position == 1) {
-            return TWO;
-        } else if (position == 2) {
+        if (position == 2) {
             return THREE;
-        } else if (position == 3) {
-            return FOUR;
-        } else {
+        } else if (position == 4) {
             return FIVE;
+        } else {
+            return ONE;
+        }
+
+    }
+
+    @Override
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+//        switch (viewType) {
+//            case ONE:
+//                return BaseViewHolder.onCreatMyViewHolder(context, parent, R.layout.discover_recyclerview_item);
+//            break;
+//            case THREE:
+//                return BaseViewHolder.onCreatMyViewHolder(context, parent, R.layout.discover_recycleview_item_two);
+//            break;
+//            case FIVE:
+//                return BaseViewHolder.onCreatMyViewHolder(context, parent, R.layout.discover_recyclerview_item_three);
+//        }
+        return BaseViewHolder.onCreatMyViewHolder(context, parent, R.layout.discover_recyclerview_item);
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+        DisCoverBean.ListBeanX listBeanX = disCoverBean.getList().get(position);
+        for (int i = 0; i < listBeanX.getList().size(); i++) {
+            holder.setOnLineImage(R.id.discover_recycleView_leftImage, listBeanX.getList().get(i).getCoverPath());
+            holder.setOnLineImage(R.id.discover_recycleView_rightImage, listBeanX.getList().get(i).getSubCoverPath());
+            holder.setText(R.id.discover_recycleView_leftText, listBeanX.getList().get(i).getTitle());
+            holder.setText(R.id.discover_recycleView_rightText, listBeanX.getList().get(i).getSubtitle());
         }
     }
 
     @Override
     public int getItemCount() {
-        return disCoverBean==null?0:disCoverBean.getList().size()
-
-                ;
+        return disCoverBean != null ? disCoverBean.getList().size() : 0;
     }
 
+
+    //    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = null;
+//        RecyclerView.ViewHolder holder = null;
+//        switch (viewType) {
+//            case ONE:
+//                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
+//                holder = new OneHolder(view);
+//               return holder;
+//            case TWO:
+//                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
+//                holder = new OneHolder(view);
+//                return holder;
+//            case THREE:
+//                view = LayoutInflater.from(context).inflate(R.layout.discover_recycleview_item_two, parent, false);
+//                holder = new TwoHolder(view);
+//                return holder;
+//            case FOUR:
+//                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item, parent, false);
+//                holder = new OneHolder(view);
+//                return holder;
+//            case FIVE:
+//                view = LayoutInflater.from(context).inflate(R.layout.discover_recyclerview_item_three, parent, false);
+//                holder = new ThreeHolder(view);
+//                return holder;
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//
+//        int type = getItemViewType(position);
+//
+//        switch (type) {
+////            case 0:
+////                OneHolder oneHolder = (OneHolder) holder;
+////                oneHolder.left.setText(disCoverBean.getList().get(0).getList().get(position).getTitle());
+////                oneHolder.right.setText(disCoverBean.getList().get(0).getList().get(position).getSubtitle());
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position).getCoverPath()).into(oneHolder.leftImgae);
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position).getSubCoverPath()).into(oneHolder.rightImage);
+////                Log.e(TAG, "onBindViewHolder: "+oneHolder.left);
+////                break;
+////            case 1:
+////                OneHolder oneHoldera = (OneHolder) holder;
+////                oneHoldera.left.setText(disCoverBean.getList().get(0).getList().get(position-1).getTitle());
+////                oneHoldera.right.setText(disCoverBean.getList().get(0).getList().get(position-1).getSubtitle());
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-1).getCoverPath()).into(oneHoldera.leftImgae);
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-1).getSubCoverPath()).into(oneHoldera.rightImage);
+////                Log.e(TAG, "onBindViewHolder: "+oneHoldera.leftImgae);
+////                break;
+////            case 2:
+////                break;
+////            case 3:
+////                OneHolder oneHolderc = (OneHolder) holder;
+////                oneHolderc.left.setText(disCoverBean.getList().get(0).getList().get(position-3).getTitle());
+////                oneHolderc.right.setText(disCoverBean.getList().get(0).getList().get(position-3).getSubtitle());
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-3).getCoverPath()).into(oneHolderc.leftImgae);
+////                Glide.with(context).load(disCoverBean.getList().get(0).getList().get(position-3).getSubCoverPath()).into(oneHolderc.rightImage);
+////                break;
+////            case 4:
+////                break;
+//
+//        }
+//    }
+//
+//
+//
+//    @Override
+//    public int getItemViewType(int position) {
+//        if (position == 0) {
+//            return ONE;
+//        } else if (position == 1) {
+//            return TWO;
+//        } else if (position == 2) {
+//            return THREE;
+//        } else if (position == 3) {
+//            return FOUR;
+//        } else {
+//            return FIVE;
+//        }
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return disCoverBean==null?0:disCoverBean.getList().size()
+//
+//                ;
+//    }
+//
     class OneHolder extends RecyclerView.ViewHolder {
         private TextView left, right;
         private ImageView leftImgae, rightImage;
