b00bd82969b02603a0d89e5e7746c7616ce7a42c
yongjie <753516036@qq.com>
2017-03-14 20:55:20 +0800

    ...

--- XMLY/app/src/main/java/com/jieleo/xmly_plus/activity/LoginActivity.java
+++ XMLY/app/src/main/java/com/jieleo/xmly_plus/activity/LoginActivity.java
@@ -6,9 +6,37 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.jieleo.xmly_plus.R;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.RequestCallback;
+import com.netease.nimlib.sdk.auth.AuthService;
+import com.netease.nimlib.sdk.auth.LoginInfo;
 
 public class LoginActivity extends BaseActivity{
     private ImageView ivBack,ivQQ,ivSina;
+
+    private String account,token;
+
+//
+    public void doLogin(){
+        LoginInfo loginInfo =new LoginInfo(account,token);
+        RequestCallback<LoginInfo> callback=new RequestCallback<LoginInfo>() {
+            @Override
+            public void onSuccess(LoginInfo param) {
+
+            }
+
+            @Override
+            public void onFailed(int code) {
+
+            }
+
+            @Override
+            public void onException(Throwable exception) {
+
+            }
+        };
+        NIMClient.getService(AuthService.class).login(loginInfo).setCallback(callback);
+    }
 
     @Override
     protected int bindLayout() {
