17e8dd8feb9d58b79faada754fccf589eae56cf6
dllo <1586676476@qq.com>
2017-03-16 19:01:05 +0800

    ..

--- XMLY/app/src/main/java/com/jieleo/xmly_plus/MyApp.java
+++ XMLY/app/src/main/java/com/jieleo/xmly_plus/MyApp.java
@@ -2,17 +2,11 @@ package com.jieleo.xmly_plus;
 
 import android.app.Application;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.os.Environment;
-import com.baidu.mapapi.SDKInitializer;
-
-import cn.sharesdk.framework.ShareSDK;
 
 /**
  * Created by liuHao on 17/3/9.
  */
-public  class MyApp extends Application{
+public class MyApp extends Application {
 
     private static Context context;
 
@@ -20,22 +14,7 @@ public  class MyApp extends Application{
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
-        ShareSDK.initSDK(context);
-        SDKInitializer.initialize(getContext());
-
-
-//        if(inMainProcess()){
-//            // 1、UI相关初始化操作
-//            // 2、相关Service调用
-//        }
-
-//        if(inMainProcess()){
-//            // 1、UI相关初始化操作
-//            // 2、相关Service调用
-//        }
     }
-
-
 
     @Override
     public void onLowMemory() {
@@ -48,7 +27,7 @@ public  class MyApp extends Application{
     }
 
 
-    public static Context getContext(){
+    public static Context getContext() {
         return context;
     }
 
