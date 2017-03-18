7ac98ffbbe4ba67280ca42462ef9edf74e254513
yongjie <753516036@qq.com>
2017-03-18 09:36:21 +0800

    。。

--- XMLY/app/src/main/java/com/jieleo/xmly_plus/fragment/UserFragment.java
+++ XMLY/app/src/main/java/com/jieleo/xmly_plus/fragment/UserFragment.java
@@ -4,17 +4,32 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+import com.jieleo.xmly_plus.MyApp;
 import com.jieleo.xmly_plus.R;
 import com.jieleo.xmly_plus.activity.LoginActivity;
 
 import com.jieleo.xmly_plus.activity.WebActivity;
 import com.xys.libzxing.zxing.activity.CaptureActivity;
 import com.xys.libzxing.zxing.encoding.EncodingUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 
 /**
@@ -23,9 +38,11 @@ import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
 
 public class UserFragment extends BaseFragment implements View.OnClickListener {
-    private ImageView headIv,scanIv,toOtherIv,createQRIv;
-    private RelativeLayout recordRl,liveRl;
-    private TextView userNameTv,introduceTv,historyTv,rssTv,downloadTv,msgTv,buyTv,walletTv,spreadTv,likeTv,freeFluxTv,moreTv,callBackTv,settingTv;
+    private ImageView headIv, scanIv, toOtherIv, createQRIv;
+    private RelativeLayout recordRl, liveRl;
+    private TextView userNameTv, introduceTv, historyTv, rssTv, downloadTv, msgTv, buyTv, walletTv, spreadTv, likeTv, freeFluxTv, moreTv, callBackTv, settingTv;
+    private RecognizerDialog mDialog;
+
     @Override
     protected int bindLayout() {
         return R.layout.fragment_user;
@@ -52,12 +69,18 @@ public class UserFragment extends BaseFragment implements View.OnClickListener {
         moreTv = (TextView) view.findViewById(R.id.tv_more_fragment_user);
         callBackTv = (TextView) view.findViewById(R.id.tv_callback_fragment_user);
         settingTv = (TextView) view.findViewById(R.id.tv_setting_fragment_user);
-        createQRIv= (ImageView) view.findViewById(R.id.iv_qr_fragment_user);
+        createQRIv = (ImageView) view.findViewById(R.id.iv_qr_fragment_user);
     }
 
     @Override
     protected void initData() {
 
+        mDialog = new RecognizerDialog(getActivity(), new InitListener() {
+            @Override
+            public void onInit(int i) {
+                Log.d("UserFragment", "i:" + i);
+            }
+        });
     }
 
     @Override
@@ -88,25 +111,52 @@ public class UserFragment extends BaseFragment implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.iv_head_fragment_user:
                 //头像的点击事件
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                final Intent intent = new Intent(getActivity(), LoginActivity.class);
                 startActivity(intent);
                 break;
             case R.id.iv_scan_fragment_user:
                 //扫描二维码的点击事件
-                Intent openCamera =new Intent(getActivity(), CaptureActivity.class);
-                startActivityForResult(openCamera,0);
+                Intent openCamera = new Intent(getActivity(), CaptureActivity.class);
+                startActivityForResult(openCamera, 0);
 
 
                 break;
             case R.id.iv_to_other_fragment_user:
                 //点击更多
-                Intent intent1=new Intent(getActivity(),LoginActivity.class);
+                final Intent intent1 = new Intent(getActivity(), LoginActivity.class);
                 getActivity().startActivity(intent1);
                 break;
             case R.id.rl_record_fragment_user:
                 //录音的点击事件
                 break;
             case R.id.rl_main_live_fragment_user:
+                mDialog.setParameter(SpeechConstant.DOMAIN, "iat");
+                mDialog.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+                mDialog.setParameter(SpeechConstant.ACCENT, "mandarin");
+                mDialog.setListener(new RecognizerDialogListener() {
+                    @Override
+                    public void onResult(RecognizerResult recognizerResult, boolean b) {
+                        Log.d("UserFragment", "b:" + b);
+                        Log.d("haha", parseIatResult(recognizerResult.getResultString()));
+                        String words = parseIatResult(recognizerResult.getResultString()).replace(" ", "");
+                        if (words.length()>=2){
+                        String URL_PART_ONE = "https://www.baidu.com/s?wd=";
+                        String URL_PART_TWO = "&rsv_spt=1&rsv_iqid=0xd973ffd5000c2260&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=5&rsv_sug1=3&rsv_sug7=100&rsv_t=21fd9Sc%2F726XvIzvOJk3ol%2FE2OhqJXusxWjpCh29zvKjK1Oj7TB7q4cMOQLRSZmXmHky&rsv_sug2=0&inputT=978&rsv_sug4=1542&rsv_sug=1";
+                        String url=URL_PART_ONE+words+URL_PART_TWO;
+                        Intent intent2=new Intent(getActivity(),WebActivity.class);
+                        intent2.putExtra("url",url);
+                        startActivity(intent2);
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onError(SpeechError speechError) {
+
+                    }
+                });
+                mDialog.show();
                 //主播中心的点击事件
                 break;
             case R.id.tv_user_name_fragment_user:
@@ -144,10 +194,10 @@ public class UserFragment extends BaseFragment implements View.OnClickListener {
                 break;
             case R.id.tv_more_fragment_user:
                 //更多的点击事件
-                String string ="https://wx.tenpay.com/f2f?t=AQAAAFpkOSeE8bc%2FsY2B%2BFFZuts%3D";
-                BitmapFactory.Options options=new BitmapFactory.Options();
-                options.inSampleSize=3;
-                Bitmap bitmap= EncodingUtils.createQRCode(string,500,500, BitmapFactory.decodeResource(getResources(),R.mipmap.imgsishuai_1,options));
+                String string = "https://wx.tenpay.com/f2f?t=AQAAAFpkOSeE8bc%2FsY2B%2BFFZuts%3D";
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 3;
+                Bitmap bitmap = EncodingUtils.createQRCode(string, 500, 500, BitmapFactory.decodeResource(getResources(), R.mipmap.imgsishuai_1, options));
                 createQRIv.setImageBitmap(bitmap);
                 break;
             case R.id.tv_callback_fragment_user:
@@ -162,12 +212,31 @@ public class UserFragment extends BaseFragment implements View.OnClickListener {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==-1){
-            Bundle bundle=data.getExtras();
-            String scanResult=bundle.getString("result");
-            Intent intent=new Intent(getActivity(), WebActivity.class);
-            intent.putExtra("url",scanResult);
+        if (resultCode == -1) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Intent intent = new Intent(getActivity(), WebActivity.class);
+            intent.putExtra("url", scanResult);
             startActivity(intent);
         }
+    }
+
+
+    public static String parseIatResult(String json) {
+        StringBuffer ret = new StringBuffer();
+        try {
+            JSONTokener tokener = new JSONTokener(json);
+            JSONObject joResult = new JSONObject(tokener);
+            JSONArray words = joResult.getJSONArray("ws");
+            for (int i = 0; i < words.length(); i++) {
+                //转写结果词，默认使用第一个结果
+                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                JSONObject obj = items.getJSONObject(0);
+                ret.append(obj.getString("w"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret.toString();
     }
 }
