dcbd3eb46becc4c58c5efc6629ccb679dc2ea975
liuHao <liuhao5059@sina.com>
2017-03-28 09:13:47 +0800

    no message

--- XMLY/app/src/main/java/com/jieleo/xmly_plus/MyApp.java
+++ XMLY/app/src/main/java/com/jieleo/xmly_plus/MyApp.java
@@ -87,7 +87,7 @@ public class MyApp extends Application {
 
         EMClient.getInstance().init(context, options);
 //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
-        EMClient.getInstance().setDebugMode(true);
+        EMClient.getInstance().setDebugMode(false);
 
 
     }
